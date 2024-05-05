fa10c6e9206164c4f29be6ebfd888eb26c4a82e8
mitumial <ace.vortex76@gmail.com>
2024-05-05 17:30:30 -0500

    add new venta class with basic structure

---
+++ venta.py
@@ -0,0 +1,24 @@
+class Venta:
+  id_venta = int
+  fecha = str
+  estado = str
+
+  def __init__(self, id_venta, fecha, estado):
+    self.id_venta = id_venta
+    self.fecha = fecha
+    self.estado = "No pagado"
+
+  def realizar_compra():
+    pass
+
+  def generar_factura():
+    pass
+
+  def calcular_total():
+    pass
+
+  def mostrar_detalles_venta():
+    pass
+
+  def cancelar_venta():
+    pass
