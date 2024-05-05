168e9712a03b40ecc4b22304490d766c19c31b44
mitumial <ace.vortex76@gmail.com>
2024-05-05 17:39:25 -0500

    create subclass Tarjeta with basic structure

--- pago.py
+++ pago.py
@@ -6,3 +6,18 @@ class Pago:
 
   def verificar_fondos():
     pass
+
+class Tarjeta(Pago):
+  cliente = Cliente
+  nro_cuenta = int
+  tipo = str
+  fecha_expiracion = str
+
+  def __init__(self, cliente, nro_cuenta, tipo, fecha_expiracion):
+    self.cliente = cliente
+    self.nro_cuenta = nro_cuenta
+    self.tipo = tipo
+    self.fecha_expiracion = fecha_expiracion
+
+  def verificar_validez():
+    pass
