f96249c5b04d49a39402d5cf953e30fc3ad66c63
sebasSalas <sebastian.salass@udea.edu.co>
2024-05-09 14:37:53 -0500

    Clase cliente creada con una estructura basica

--- cliente.py
+++ cliente.py
@@ -22,9 +22,20 @@ class Cliente:
         self.celular = celular
         self.vehiculos_comprados = vehiculos_comprados
         
-    def ingresar_cliente():
-        pass
-    
+    def ingresar_cliente(self):
+        print("-"*30)
+        print("INGRESAR DATOS DEL CLIENTE:")
+        self.id_cliente = int(input("Ingrese la id del cliente: \n"))
+        self.nombre = input("Ingrese los nombres del cliente: \n")
+        self.apellidos = input("Ingrese los apellidos del cliente: \n")
+        self.documento = int(input("Ingrese el numero de documento del cliente: \n"))
+        self.edad = int(input("Ingrese la edad del cliente: \n"))
+        self.genero = input("Ingrese el genero del cliente : (Masculino/Femenino): \n")
+        self.direccion = int(input("Ingrese la dirección dle cliente: \n"))
+        self.email = input("Ingrese la direccion de correo electronico del cliente: \n")
+        self.celular = int(input("Ingrese el número de celular del cliente: \n"))
+        self.vehiculos_comprados = []
+        
     def mostrar_detalles_cliente():
         pass
     
