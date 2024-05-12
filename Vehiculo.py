84d66d4aa3301efc6553673a3b1a4ad0bf075e16
alvaroj04 <alvaro.osuna@udea.edu.co>
2024-05-12 17:06:45 -0500

    clasevehiculo

--- Vehiculo.py
+++ Vehiculo.py
@@ -1,18 +1,32 @@
 class Vehiculo:
-    
-    def __init__(self, id_vehiculo=None, marca=None, modelo=None, año=None, placa=None, color=None, combustible=None, transmision=None, cilindraje=None, kilometraje=None, puertas=None, alarma=bool, sensor=bool, precio=None):
-        self.id_vehiculo= id_vehiculo
-        self.marca= marca
-        self.modelo= modelo
-        self.año= año
-        self.placa= placa
-        self.color= color
-        self.combustible= combustible
-        self.transmision= transmision
-        self.cilindraje= cilindraje
-        self.kilometraje= kilometraje
-        self.puertas= puertas
-        self.alarma= alarma
-        self.sensor= sensor
-        self.precio = precio
-        pass
+        id_vehiculo= int
+        marca= str
+        modelo= str
+        año= int
+        placa= str
+        color= str
+        combustible= str
+        transmision= str
+        cilindraje= float
+        kilometraje= float
+        puertas= int
+        alarma= bool
+        sensor= bool
+        precio = float
+
+        def __init__(self, id_vehiculo, marca, modelo, año, placa, color, combustible, transmision, cilindraje, kilometraje, puertas, alarma, sensor, precio) -> None:
+            self.id_vehiculo= id_vehiculo
+            self.marca= marca
+            self.modelo= modelo
+            self.año= año
+            self.placa= placa
+            self.color= color
+            self.combustible= combustible
+            self.transmision= transmision
+            self.cilindraje= cilindraje
+            self.kilometraje= kilometraje
+            self.puertas= puertas
+            self.alarma= alarma
+            self.sensor= sensor
+            self.precio = precio
+            pass
