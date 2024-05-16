2bb419f10f52422ef698b362011ed492227f5913
mitumial <ace.vortex76@gmail.com>
2024-05-16 18:18:25 -0500

    add attribute values to class Venta, add method show details of sale

--- venta.py
+++ venta.py
@@ -1,30 +1,72 @@
+from datetime import datetime as dt
+import uuid
+import re
+
+
 class Cliente:
-  pass
+    pass
+
 
 class Vehiculo:
-  pass
+    pass
+
 
 class Venta:
-  id_venta = int
-  fecha = str
-  estado = str
+    _id_venta = str
+    _fecha = str
+    _estado = str
+    cliente = Cliente
+    vehiculo = Vehiculo
 
-  def __init__(self, id_venta, fecha):
-    self.id_venta = id_venta
-    self.fecha = fecha
-    self.estado = "No pagado"
+    def __init__(self, cliente=None, vehiculo=None):
+        self._id_venta = str(uuid.uuid4())
+        self._fecha = dt.now().strftime("%d-%m-%Y")
+        self._estado = "No pagado"
+        self.cliente = cliente
+        self.vehiculo = vehiculo
 
-  def realizar_compra():
-    pass
+    @property
+    def fecha(self):
+        return self._fecha
 
-  def generar_factura():
-    pass
+    @fecha.setter
+    def fecha(self, value):
+        if not re.match(r"\d{2}-\d{2}-\d{4}", value):
+            raise ValueError("La fecha debe estar en formato dd-mm-yyyy")
+        else:
+            self._fecha = value
 
-  def calcular_total():
-    pass
+    def realizar_compra(self):
+        self.cliente
+        self.vehiculo
 
-  def mostrar_detalles_venta():
-    pass
+    def generar_factura(self):
+        pass
 
-  def cancelar_venta():
-    pass
+    def calcular_total(self):
+        pass
+
+    def mostrar_detalles_venta(self):
+        print(
+            f"""\n
+                Cliente:{self.cliente.nombres} {self.cliente.apellidos}
+                \n
+                Vehiculo:{self.vehiculo.marca} {self.vehiculo.modelo}
+                \n
+                Fecha:{self._fecha}
+                \n
+                Estado:{self._estado}
+                \n
+                Por la suma total de: {self.vehiculo.precio}
+                \n
+            """
+        )
+
+    def cancelar_venta(self):
+        pass
+
+
+mySale = Venta()
+
+mySale.fecha = "11-15-20"
+print(mySale.fecha)
